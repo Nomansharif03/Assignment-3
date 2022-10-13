00c03bdbbf0d2c4daa158f0f3292e4c37483b910
Nomansharif03 <115030854+Nomansharif03@users.noreply.github.com>
2022-10-12 23:33:08 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+
+{
+    public class candidate
+
+{
+    public string name;
+    public string cnic;
+    public double cgpa;
+    public string location;
+    
+
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var candidate = new List<candidate>()
+            {
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"}, 
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"}, 
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"}, 
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"}, 
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"},
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"}, 
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} 
+            };
+
+                
+                Console.WriteLine("Interested Candidates with their data are : ");
+                for(int i=0;i<candidate.Count;i++)
+                {
+                    Console.WriteLine("Candidate names are:  "+ candidate[i].name);
+                    Console.WriteLine("Candidate cnic are:  " + candidate[i].cnic);
+                    Console.WriteLine("Candidate cgpa are:  " + candidate[i].cgpa);
+                    Console.WriteLine("Candidate location are:  " + candidate[i].location);
+
+                }
+            Console.ReadLine();
+            }
+     
+        }
+    
+    }
+
+
