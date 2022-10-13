c5525016b9c77655df3d15d64abb2f1f8031c293
Nomansharif03 <115030854+Nomansharif03@users.noreply.github.com>
2022-10-13 01:56:47 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,19 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task1
-
+namespace Task3
 {
     public class candidate
+    {
+        public string name;
+        public string cnic;
+        public double cgpa;
+        public string location;
+        public double Project1Marks;
+        public double Project2Marks;
+        public double ProjectTotalMarks;
 
-{
-    public string name;
-    public string cnic;
-    public double cgpa;
-    public string location;
-    
 
-}
+
+    }
     class Program
     {
         static void Main(string[] args)
@@ -24,37 +26,61 @@ namespace Task1
             var candidate = new List<candidate>()
             {
                 new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"}, 
-                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"}, 
-                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"}, 
-                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"}, 
-                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"},
-                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"}, 
+                new candidate(){name=" Ali Akbar", cnic="1610115218935", cgpa=2.9, location="KPK"}, 
+                new candidate(){name=" Jammu Khan", cnic="1610115218935", cgpa=3.4, location="KPK"}, 
+                new candidate(){name=" Saad Khan", cnic="1610115218935", cgpa=3.07, location="KPK"}, 
+                new candidate(){name=" Shahid Ali", cnic="1610115218935", cgpa=2.07, location="KPK"},
+                new candidate(){name=" Afridi", cnic="1610115218935", cgpa=1.07, location="KPK"}, 
+                new candidate(){name=" Shahid khan", cnic="1610115218935", cgpa=3.80, location="KPK"} ,
+                new candidate(){name=" Sarfaraz", cnic="1610115218935", cgpa=3.97, location="KPK"} ,
+                new candidate(){name=" Gujjar Khan", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=2.97, location="KPK"} ,
                 new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
-                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
-                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
-                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
-                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
-                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
-                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
-                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
-                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.07, location="KPK"} 
+                new candidate(){name=" Wahif", cnic="1610115218935", cgpa=3.07, location="KPK"} ,
+                new candidate(){name=" Fazal", cnic="1610115218935", cgpa=2.87, location="KPK"} ,
+                new candidate(){name=" Raheem", cnic="1610115218935", cgpa=3.17, location="KPK"} ,
+                new candidate(){name=" Shahid", cnic="1610115218935", cgpa=3.97, location="KPK"} 
             };
 
-                
-                Console.WriteLine("Interested Candidates with their data are : ");
-                for(int i=0;i<candidate.Count;i++)
+
+            Console.WriteLine("Candidates with their CGPA Greate than 3.00  are : ");
+            Random rnd = new Random();
+            for (int i = 0; i < candidate.Count; i++)
+            {
+                if (candidate[i].cgpa >= 3)
                 {
-                    Console.WriteLine("Candidate names are:  "+ candidate[i].name);
-                    Console.WriteLine("Candidate cnic are:  " + candidate[i].cnic);
-                    Console.WriteLine("Candidate cgpa are:  " + candidate[i].cgpa);
-                    Console.WriteLine("Candidate location are:  " + candidate[i].location);
+                    Console.WriteLine("Candidate names are:  " + candidate[i].name);
+                    Console.WriteLine("cnic:  " + candidate[i].cnic);
+                    Console.WriteLine("cgpa:  " + candidate[i].cgpa);
+                    Console.WriteLine("location" + candidate[i].location);
+                    candidate[i].Project1Marks = rnd.Next(80, 101);
+                    candidate[i].Project2Marks = rnd.Next(80, 101);
+                    candidate[i].ProjectTotalMarks = candidate[i].Project1Marks + candidate[i].Project2Marks;
+
+                    if (candidate[i].ProjectTotalMarks > 180)
+                    {
+                        Console.WriteLine("Passed");
+                    }
+
+
+
 
                 }
-            Console.ReadLine();
             }
-     
+            Console.WriteLine("Candidates that had marks above 90% is given by following : ");
+            for (int i = 0; i < candidate.Count; i++)
+            {
+                if (candidate[i].ProjectTotalMarks >= 180)
+                {
+                    Console.WriteLine("Congratulations Mr.  " + candidate[i].name + "   You have scored above 90% in the projects");
+
+                }
+            }
+            Console.ReadLine();
         }
-    
+
     }
+
+}
 
 
